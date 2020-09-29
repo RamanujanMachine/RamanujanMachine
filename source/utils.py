dae8c5e418662bd7162992eca1a40161f06ec3f6
Jellybean <rotelimelech@gmail.com>
2020-09-29 13:06:48 +0300

    adding usefull functions to utils- get_poly_deg, series generator function and gcf plotter

--- source/utils.py
+++ source/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from typing import List
 import time
+import mpmath
+import matplotlib.pyplot as plt
 
 # Measures the amount of time the function takes to run in milliseconds in order to check improvements
 def measure_performance(func):
@@ -40,5 +42,90 @@ def find_polynomial_series_coefficients(poly_deg, lead_terms: List[int], startin
         return list(int_coefs.astype(int))
 
 
-# an_coefs = [n * (n * (6 * n + 9) + 5) + 1 for n in range(10)]
-# print(find_polynomial_series_coefficients(3, an_coefs, 0))
+def get_poly_deg_and_leading_coef(poly_coef):
+    deg = len(poly_coef) - 1
+    for i in poly_coef:
+        if i != 0:
+            return deg, i
+        else:
+            deg -= 1
+    # if we get here the degree is 0
+    return deg, poly_coef[-1]
+
+
+def iter_series_items_from_compact_poly(poly_a, max_runs, starting_n=0):
+    """
+    create a series of type n(n(...(a[0]*n + a[1]) + a[2]) + ...) + a[k]
+    :param poly_a: a[k] coefficients
+    :param n: length of series
+    :return: a list of numbers in series
+    """
+    for i in range(starting_n, max_runs + starting_n):
+        tmp = 0
+        for c in poly_a:
+            tmp *= i
+            tmp += c
+        yield tmp
+
+
+def plot_gcf_convergens(an_poly_coef, bn_poly_coef, max_iters, divide_interval=101, label=None):
+    computed_values = []
+    label = f'an {an_poly_coef} bn {bn_poly_coef}' if not label else label
+
+    # a_n iterator is always one ahead, while a[0] goes to p. so this is 
+    # an ugly hack
+
+    poly_a_deg, _ = get_poly_deg_and_leading_coef(an_poly_coef)
+    poly_b_deg, _ = get_poly_deg_and_leading_coef(bn_poly_coef)
+
+    print(f'deg a - {poly_a_deg} deg b - {poly_b_deg}')
+    if poly_a_deg * 2 == poly_b_deg:
+        print('\texpo')
+        print(f'\t4b_d = {bn_poly_coef[0] * 4} | -a^2 = {-1 * (an_poly_coef[0]**2)}')
+        if bn_poly_coef[0] * 4 > -1 * (an_poly_coef[0]**2):
+            print('\t\tcond passed')
+        elif bn_poly_coef[0] * 4 == -1 * (an_poly_coef[0]**2):
+            print('\t\tequality')
+        else:
+            print('\t\tcond failed')
+    elif poly_a_deg * 2 > poly_b_deg:
+        print('\tsuper expo')
+    else:
+        print('\tsub expo')
+    an_items_iterator = iter_series_items_from_compact_poly(an_poly_coef, max_iters)
+    bn_items_iterator = iter_series_items_from_compact_poly(bn_poly_coef, max_iters, 1)
+    
+    prev_q = 0
+    q = 1
+    prev_p = 1
+    p = an_items_iterator.__next__() # will place a[0] to p
+
+    for i, (a_i_1, b_i) in enumerate(zip(an_items_iterator, bn_items_iterator)):
+        # a_i_1 is the (i+1)'th item of an, and b_i the the i'th item of bn
+        tmp_a = q
+        tmp_b = p
+
+        q = a_i_1 * q + b_i * prev_q
+        p = a_i_1 * p + b_i * prev_p
+        
+        prev_q = tmp_a
+        prev_p = tmp_b
+
+        # This is the hard part to compute.
+        # TODO - consider computing once in every X iters; Might be significant 
+        if i % divide_interval != 0:
+            continue
+        if q != 0:  # safety check
+            computed_values.append((mpmath.mpf(p) / mpmath.mpf(q), i))
+        else:
+            computed_values.append((mpmath.mpf(0), i))
+
+    global current_fig
+    current_fig += 1 
+    plt.figure(current_fig)
+    plt.plot([i[1] for i in computed_values], [i[0] for i in computed_values], '+')
+    plt.ion()
+    plt.title(label=label)
+    plt.show()
+
+    return computed_values
