36197df1fae4c68a0c578a1ca016e8d86fab0d2d
Jellybean <rotelimelech@gmail.com>
2021-01-15 21:50:49 +0200

    adding paper results

--- scripts/paper_results/pi_results.py
+++ scripts/paper_results/pi_results.py
@@ -4,17 +4,17 @@ from ramanujan.enumerators.EfficentGCFEnumerator import *
 from ramanujan.poly_domains.CartesianProductPolyDomain import *
 
 # create a LHS table for e
-saved_hash = 'e_lhs_dept5_db'
-lhs_search_limit = 5
+saved_hash = 'pi.lhs.dept20.db'
+lhs_search_limit = 20
 lhs = LHSHashTable(
     saved_hash,
     lhs_search_limit,
-    [g_const_dict['e']]) 
+    [g_const_dict['pi']]) 
 
 # define a poly domain to iter through
 poly_search_domain = CartesianProductPolyDomain(
-	2, [-5,5], # an deg 2, coefs ranging from -5 to 5
-	2, [-5,5]) # bn deg 2, coefs ranging from -5 to 5
+	2, [-13,13], # an deg 2, coefs ranging from -5 to 5
+	3, [-11,11]) # bn deg 2, coefs ranging from -5 to 5
 
 # create an enumeator to iter thought the poly domain and compare it to the 
 # lhs table
--- scripts/paper_results/zeta3_results.py
+++ scripts/paper_results/zeta3_results.py
@@ -1,27 +1,28 @@
 import sympy
 from ramanujan.LHSHashTable import *
 from ramanujan.enumerators.EfficentGCFEnumerator import *
-from ramanujan.poly_domains.CartesianProductPolyDomain import *
+from ramanujan.poly_domains.Zeta3Domain1 import *
 
 # create a LHS table for e
-saved_hash = 'e_lhs_dept5_db'
-lhs_search_limit = 5
+saved_hash = 'zeta3.lhs.dept20.db'
+lhs_search_limit = 20
 lhs = LHSHashTable(
     saved_hash,
     lhs_search_limit,
-    [g_const_dict['e']]) 
+    [g_const_dict['zeta'](3)]) 
 
 # define a poly domain to iter through
-poly_search_domain = CartesianProductPolyDomain(
-	2, [-5,5], # an deg 2, coefs ranging from -5 to 5
-	2, [-5,5]) # bn deg 2, coefs ranging from -5 to 5
+poly_search_domain = Zeta3Domain1(
+	[(2,2), (1,1), (-50,50), (-50,50), # an coefs
+	(-10,-1) # bn coef
+	]) 
 
 # create an enumeator to iter thought the poly domain and compare it to the 
 # lhs table
 enumerator = EfficentGCFEnumerator(
 	lhs,
 	poly_search_domain,
-	[g_const_dict['e']],
+	[g_const_dict['zeta'](3)],
 	lhs_search_limit
 	)
 
