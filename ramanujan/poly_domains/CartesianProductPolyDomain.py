63f3110ee5d7455a19505e3b61866f3dfc626df3
Jellybean <rotelimelech@gmail.com>
2021-01-16 18:01:51 +0200

    changed the way domain is stored to a list of coef ranges
    this will allow us to overrding logic more easily

--- ramanujan/poly_domains/CartesianProductPolyDomain.py
+++ ramanujan/poly_domains/CartesianProductPolyDomain.py
@@ -8,10 +8,12 @@ class CartesianProductPolyDomain(AbstractPolyDomains):
 	"""
 	def __init__(self, a_deg, a_coef_range, b_deg, b_coef_range, 
 		*args, **kwargs):
-		self.a_deg = a_deg 
-		self.a_coef_range = a_coef_range
+		self.a_deg = a_deg
+		# expanding the range to a different range for each coef
+		# allows us to use the same functions for decedent classes
+		self.a_coef_range = [a_coef_range for i in range(a_deg + 1)]
 		self.b_deg = b_deg
-		self.b_coef_range = b_coef_range
+		self.b_coef_range = [b_coef_range for i in range(b_deg + 1)]
 
 		self.an_length = self.get_an_length()
 		self.bn_length = self.get_bn_length()
@@ -30,17 +32,17 @@ class CartesianProductPolyDomain(AbstractPolyDomains):
 			size *= self._range_size(var_range)
 		return  size
 
-	def expand_var_ranges_to_domain(self, var_ranges):
-		return [ \
-			[i for i in range(coef[0], coef[1] + 1)] \
-				for coef in var_ranges
-		]
+	# def expand_var_ranges_to_domain(self, var_ranges):
+	# 	return [ \
+	# 		[i for i in range(coef[0], coef[1] + 1)] \
+	# 			for coef in var_ranges
+	# 	]
 
 	def get_an_length(self):
-		return (self.a_coef_range[1] - self.a_coef_range[0] + 1) ** (self.a_deg + 1)
-	
+		return self.domain_size_by_var_ranges(self.a_coef_range)
+
 	def get_bn_length(self):
-		return(self.b_coef_range[1] - self.b_coef_range[0] + 1) ** (self.b_deg + 1)
+		return self.domain_size_by_var_ranges(self.b_coef_range)
 
 	def get_calculation_method(self):
 		# both an and bn are regular compact polys
@@ -48,11 +50,15 @@ class CartesianProductPolyDomain(AbstractPolyDomains):
 			iter_series_items_from_compact_poly
 
 	def dump_domain_ranges(self):
-		an_domain = [[i for i in range(self.a_coef_range[0], self.a_coef_range[1]+1)] \
-			for i in range(self.a_deg + 1)]
+		an_domain = [ \
+			[i for i in range(coef[0], coef[1] + 1)] \
+				for coef in self.a_coef_range
+		]
 
-		bn_domain = [[i for i in range(self.b_coef_range[0], self.b_coef_range[1]+1)] \
-			for i in range(self.b_deg + 1)]
+		bn_domain = [ \
+			[i for i in range(coef[0], coef[1] + 1)] \
+				for coef in self.b_coef_range
+		]
 
 		return an_domain, bn_domain
 
