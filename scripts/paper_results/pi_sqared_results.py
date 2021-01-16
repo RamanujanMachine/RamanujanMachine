e83a225cd6009dd1182080c558d0dd8bfd3e6d0b
Jellybean <rotelimelech@gmail.com>
2021-01-16 18:24:22 +0200

    updating scripts to match recent changes

--- scripts/paper_results/pi_sqared_results.py
+++ scripts/paper_results/pi_sqared_results.py
@@ -1,27 +1,29 @@
 import sympy
 from ramanujan.LHSHashTable import *
 from ramanujan.enumerators.EfficentGCFEnumerator import *
-from ramanujan.poly_domains.CartesianProductPolyDomain import *
+from ramanujan.poly_domains.Zeta3Domain1 import *
+from constants import g_const_dict
 
 # create a LHS table for e
-saved_hash = 'pi.lhs.dept20.db'
+saved_hash = 'pi_sqared.lhs.dept20.db'
 lhs_search_limit = 20
 lhs = LHSHashTable(
     saved_hash,
     lhs_search_limit,
-    [g_const_dict['pi']]) 
+    [g_const_dict['zeta'](3)]) 
 
 # define a poly domain to iter through
-poly_search_domain = CartesianProductPolyDomain(
-	1, [-13,13], # an deg 1, coefs ranging from -5 to 5
-	2, [-11,11]) # bn deg 2, coefs ranging from -5 to 5
+poly_search_domain = Zeta3Domain1(
+	[(2,2), (1,1), (-50,50), (-50,50), # an coefs
+	(-10,-1) # bn coef
+	]) 
 
 # create an enumeator to iter thought the poly domain and compare it to the 
 # lhs table
 enumerator = EfficentGCFEnumerator(
 	lhs,
 	poly_search_domain,
-	[g_const_dict['pi']],
+	[g_const_dict['zeta'](3)],
 	lhs_search_limit
 	)
 
--- scripts/paper_results/zeta3_results.py
+++ scripts/paper_results/zeta3_results.py
@@ -3,7 +3,7 @@ from ramanujan.LHSHashTable import *
 from ramanujan.enumerators.EfficentGCFEnumerator import *
 from ramanujan.poly_domains.Zeta3Domain1 import *
 
-# create a LHS table for e
+# create a LHS table for zeta
 saved_hash = 'zeta3.lhs.dept20.db'
 lhs_search_limit = 20
 lhs = LHSHashTable(
@@ -13,9 +13,9 @@ lhs = LHSHashTable(
 
 # define a poly domain to iter through
 poly_search_domain = Zeta3Domain1(
-	[(2,2), (1,1), (-50,50), (-50,50), # an coefs
+	[(2,2), (1,1), (-50,50), (-50,50)], # an coefs
 	(-10,-1) # bn coef
-	]) 
+	) 
 
 # create an enumeator to iter thought the poly domain and compare it to the 
 # lhs table
