33c8d7d3188f07471dc521ed455d1ca533ddc45a
rotelimelech <rotelimelech@gmail.com>
2021-01-17 02:14:30 +0200

    cleaning depricated functions, and updating comments

--- ramanujan/poly_domains/AbstractPolyDomains.py
+++ ramanujan/poly_domains/AbstractPolyDomains.py
@@ -2,24 +2,24 @@ from abc import ABCMeta
 
 class AbstractPolyDomains(metaclass=ABCMeta):
 	"""
-	This is an abstract class, ment to represent the polynomial domains
-	iterated throught a run.
+	This is an abstract class, meant to represent the polynomial domains
+	iterated through a run.
 
-	A poly domain is defined here as two families of polynomials. Each poly
-	is defined by the degrees of freedom, and their resprected ranges in the 
-	family. This class will generate all sutible constants to fill those 
-	degrees of freedom. One must also specify a way to subsitude those 
-	constants, as the calculation method
+	A poly domain is defined here as two families of polynomials. Each family
+	is defined by a function with variables, referred to as coefs. Each
+	coef has it allowed range size.
+	This class will generate all coefs under those restrictions. Each decanted 
+	class must also create a calculation method to use those coefs
 
 	Examples:
 		A(n) = a*n^2 + b*n + c
 		B(n) = d*n^2 + e*n + f
-		is clasical a poly domain, where both a,b has 6 deg of freedom 
+		is classical a poly domain, where both a,b has 6 deg of freedom 
 
 		A(n) = a*n^2 + b*n + c
 		B(n) = A(n) * A(n-1)
 		This combination only has 3 degrees of freedom. Also, given a,b,c
-		the calculation for A and B are diffrenet. The way to calculate each
+		the calculation for A and B are different. The way to calculate each
 		series by given constants, is the calculation method
 	"""
 	def __init__(self):
