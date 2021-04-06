78542a16caf1d8f944838334fef253c284d51940
Rotem Elimelech <rotelimelech@gmail.com>
2021-04-06 12:50:20 +0300

    cosmetics & pep8

--- ramanujan/multiprocess_enumeration.py
+++ ramanujan/multiprocess_enumeration.py
@@ -8,14 +8,12 @@ class Dummy(object):
     To pass less data to each process, we'll be using this class to duplicate only
     the bloom filter. 
     Also, passing LHSHashTable to a child process requires us to pickle it, which creates
-    problems. This way, we only handle the required data when spawnning a new process
+    problems. This way, we only handle the required data when spawning a new process
     """
     pass
 
 
 def _single_process_execution(enumerator_class, lhs, poly_search_domain, const_vals):
-    import os
-    print(os.getpid())
     enumerator = enumerator_class(
         lhs,
         poly_search_domain,
@@ -23,17 +21,19 @@ def _single_process_execution(enumerator_class, lhs, poly_search_domain, const_v
     
     return enumerator.find_initial_hits()
 
+
 def multiprocess_enumeration(enumerator_class, lhs, poly_search_domain, const_vals, number_of_processes):
     """
-    This function will split an execution to number_of_processes different processes
-    The poly_domain will be splitted, and for each chunk an instance of lhs and 
-    enumerator will be created. Each instance will preform the first enumeration.
-    The refinning process requires to load the LHS dict to memory. This will be done by
-    only one instance, when all first enumerations are finnished.
+    This function will split an execution to number_of_processes different processes the poly_domain will be split, and
+    for each chunk an instance of lhs and enumerator will be created. Each instance will preform the first enumeration.
+    The refining process requires to load the LHS dict to memory. This will be done by  only one instance, when all
+    first enumerations are finished.
 
     :param enumerator_class: the CLASS (NOT an instance) of the requested enumerator
     :param lhs: an LHSHashTable object
-    :param poly_search_domain: the requested PolyDomain - this object will be splitted to processes
+    :param poly_search_domain: the requested PolyDomain - this object will be split to processes
+    :param const_vals: A list with the requested constants. This will be passed as-is to the enumerator
+    :param number_of_processes: Number of processes to spawn
     """
 
     pool = multiprocessing.Pool(processes=number_of_processes)
@@ -44,8 +44,8 @@ def multiprocess_enumeration(enumerator_class, lhs, poly_search_domain, const_va
     lean_lhs.bloom = lhs.bloom
 
     # Creating arguments for each process function
-    splitted_domain = poly_search_domain.split_domains_to_processes(number_of_processes)
-    for domain_chunk in splitted_domain:
+    split_domain = poly_search_domain.split_domains_to_processes(number_of_processes)
+    for domain_chunk in split_domain:
         arguments.append((
             enumerator_class,
             deepcopy(lean_lhs.bloom),
