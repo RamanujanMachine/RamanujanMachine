4640527e4980bc37d194a84c82b2ebda3da5573c
Rotem Elimelech <rotelimelech@gmail.com>
2021-04-18 10:54:23 +0300

    moving calculating func's defauts values to consts

--- ramanujan/enumerators/RelativeGCFEnumerator.py
+++ ramanujan/enumerators/RelativeGCFEnumerator.py
@@ -8,6 +8,10 @@ from ramanujan.constants import g_N_verify_terms, g_N_verify_compare_length, g_N
 from .AbstractGCFEnumerator import AbstractGCFEnumerator, Match, RefinedMatch
 
 IterationMetadata = namedtuple('IterationMetadata', 'an_coef bn_coef')
+FIRST_STEP_MIN_ITERS = 7
+FIRST_STEP_BURST_NUMBER = 7
+SECOND_STEP_MIN_ITERS = 7
+SECOND_STEP_BURST_NUMBER = 7
 
 
 class ZeroInAn(Exception):
@@ -26,7 +30,7 @@ def trunc_division(p, q):
     return -div if sign else div
 
 
-def gcf_calculation_to_precision(an_iterator, bn_iterator, result_precision, min_iters=7, burst_number=7):
+def gcf_calculation_to_precision(an_iterator, bn_iterator, result_precision, min_iters, burst_number):
     """
     Calculate the GCF's value to a chaning dept that depends on the GCF's convergence rate.
 
@@ -37,14 +41,14 @@ def gcf_calculation_to_precision(an_iterator, bn_iterator, result_precision, min
     Since hugh int division is a costly process, we'll do so only every burst_number of calculations.
     """
     computed_values = []
-    items_computed = 0 
+    items_computed = 0
 
     # Some GCFs converge by oscillation. its better to take odd burst_number to test
     # values from both sub-series
     burst_number = burst_number if burst_number % 2 == 1 else burst_number + 1
     next_gcf_calculation = burst_number if burst_number >= min_iters else min_iters
 
-    precision_factor = 10**result_precision
+    precision_factor = 10 ** result_precision
     prev_q = 0
     q = 1
     prev_p = 1
@@ -65,7 +69,7 @@ def gcf_calculation_to_precision(an_iterator, bn_iterator, result_precision, min
 
         q = a_i * q + b_i * prev_q
         p = a_i * p + b_i * prev_p
-        
+
         prev_q = tmp_a
         prev_p = tmp_b
 
@@ -126,7 +130,7 @@ class RelativeGCFEnumerator(AbstractGCFEnumerator):
 
                 yield (
                     an_cache.iter_series_items(max_iters=max_iters),
-                    bn_cache[bn_coefs].iter_series_items(max_iters=max_iters), 
+                    bn_cache[bn_coefs].iter_series_items(max_iters=max_iters),
                     IterationMetadata(an_coefs, bn_coefs))
 
         else:  # cache an
@@ -140,7 +144,7 @@ class RelativeGCFEnumerator(AbstractGCFEnumerator):
 
                 yield (
                     an_cache[an_coefs].iter_series_items(max_iters=max_iters),
-                    bn_cache.iter_series_items(max_iters=max_iters), 
+                    bn_cache.iter_series_items(max_iters=max_iters),
                     IterationMetadata(an_coefs, bn_coefs))
 
     def _first_enumeration(self, verbose: bool):
@@ -150,7 +154,8 @@ class RelativeGCFEnumerator(AbstractGCFEnumerator):
         next_status_print = 100_000
         for i, (an_iter, bn_iter, metadata) in enumerate(self._iter_domains_with_cache(100)):
             try:
-                key = gcf_calculation_to_precision(an_iter, bn_iter, g_N_initial_key_length)
+                key = gcf_calculation_to_precision(an_iter, bn_iter, g_N_initial_key_length, FIRST_STEP_MIN_ITERS,
+                                                   FIRST_STEP_BURST_NUMBER)
             except (ZeroInAn, NotConverging, ZeroDivisionError):
                 continue
 
@@ -164,7 +169,7 @@ class RelativeGCFEnumerator(AbstractGCFEnumerator):
                     f'({round(100. * i / self.poly_domains.num_iterations, 2)}%). ' +
                     f' found so far {len(results)} results')
                 print(f'currently at an = {metadata.an_coef} bn = {metadata.bn_coef}')
- 
+
         if verbose:
             print(f'created results after {time() - start}s')
             print(f'found {len(results)} results')
@@ -178,7 +183,7 @@ class RelativeGCFEnumerator(AbstractGCFEnumerator):
         precise_results = []
         counter = 0
         n_iterations = len(intermediate_results)
-        key_factor = 10**g_N_verify_compare_length
+        key_factor = 10 ** g_N_verify_compare_length
         an_series_iter, bn_series_iter = self.poly_domains.get_calculation_method()
 
         for res in intermediate_results:
@@ -187,13 +192,14 @@ class RelativeGCFEnumerator(AbstractGCFEnumerator):
                 print('Calculated {} matches out of {} to a more precise value.'.format(
                     counter, n_iterations))
 
-            an_iter = an_series_iter(res.rhs_an_poly, g_N_verify_terms*2, start_n=0)
-            bn_iter = bn_series_iter(res.rhs_bn_poly, g_N_verify_terms*2, start_n=0)
-            long_key = gcf_calculation_to_precision(an_iter, bn_iter, g_N_verify_compare_length, min_iters=100,
-                                                    burst_number=51)
+            an_iter = an_series_iter(res.rhs_an_poly, g_N_verify_terms * 2, start_n=0)
+            bn_iter = bn_series_iter(res.rhs_bn_poly, g_N_verify_terms * 2, start_n=0)
+            long_key = gcf_calculation_to_precision(an_iter, bn_iter, g_N_verify_compare_length,
+                                                    min_iters=SECOND_STEP_MIN_ITERS,
+                                                    burst_number=SECOND_STEP_BURST_NUMBER)
             rhs_val = mpmath.mpf(long_key) / key_factor
             rhs_str = mpmath.nstr(rhs_val, g_N_verify_compare_length)
-            
+
             precise_results.append((res, rhs_str))
         return precise_results
 
@@ -202,7 +208,7 @@ class RelativeGCFEnumerator(AbstractGCFEnumerator):
         This step is identical to the one in Efficient enumerator. The two we're quite different in the
         rest of the implementation, so it didn't feel right to extend EfficientGCFEnumerator in this class.
         Hence, there is some code duplication here
-        
+
         validate intermediate results to 100 digit precision
         :param precise_intermediate_results:  list of results from first enumeration
         :param verbose: if true print status.
@@ -214,7 +220,6 @@ class RelativeGCFEnumerator(AbstractGCFEnumerator):
         constant_vals = [const() for const in self.constants_generator]
 
         for res, rhs_str in precise_intermediate_results:
-
             counter += 1
             if (counter % 10_000 == 0 or counter == n_iterations) and verbose:
                 print('Passed {} permutations out of {}. Found so far {} matches'.format(
@@ -235,7 +240,7 @@ class RelativeGCFEnumerator(AbstractGCFEnumerator):
             for i, match in enumerate(all_matches):
                 # TODO - trunc_division will flat to 0, while nstr will do the right thing
                 # this forces the value to be flatted to zero.
-                val_str = mpmath.nstr(match[0], g_N_verify_compare_length+1)[:-1]
+                val_str = mpmath.nstr(match[0], g_N_verify_compare_length + 1)[:-1]
                 if val_str == rhs_str:
                     # This patch is meant to allow support for multiple matches for an
                     # LHS key, i will later be used to determine which item in the LHS dict
