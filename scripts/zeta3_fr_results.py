e22e6fa948d6f6792699e5581a8ae06d7a9b43b0
Rotem Elimelech <rotelimelech@gmail.com>
2021-06-01 15:28:03 +0300

    adding example for the FR algorithm

---
+++ scripts/zeta3_fr_results.py
@@ -0,0 +1,29 @@
+from ramanujan.LHSHashTable import LHSHashTable
+from ramanujan.enumerators.FREnumerator import FREnumerator
+from ramanujan.poly_domains.Zeta3Domain2 import Zeta3Domain2
+from ramanujan.constants import g_const_dict
+
+"""
+This script enumerates GCFs for zeta(3) using the FR algorithem.
+It uses a specific family of series that tends to generate conjectures for zeta(3)
+The family is defined under ramanujan.poly_domains.Zeta3Domain3
+
+It uses the following series:
+a(n) = x0(n**3 + (n+1)**3) + x1(2n+1)
+b(n) = x2*n^6
+"""
+ 
+# define the poly domain
+poly_search_domain = Zeta3Domain2(
+    [(1, 3), (-20, 20)],
+    (-4, -1))
+
+# create an enumerator to iter thought the poly domain and compare it to the lhs table
+enumerator = FREnumerator(
+    poly_search_domain,
+    [g_const_dict['zeta'](3)]
+)
+
+results = enumerator.full_execution()
+print("{} results found!".format(len(results)))
+print(results)
