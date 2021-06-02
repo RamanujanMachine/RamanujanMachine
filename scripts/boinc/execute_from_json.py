bc41544e15422f0769d86d879fdfbed03eaeba27
Rotem Elimelech <rotelimelech@gmail.com>
2021-06-02 23:41:05 +0300

    allowing only FR enumerator

--- scripts/boinc/execute_from_json.py
+++ scripts/boinc/execute_from_json.py
@@ -1,27 +1,24 @@
-import json
 import os
 import sys
 import json
-import time
-import ramanujan
-import pybloom_live
 from ramanujan.poly_domains.Zeta3Domain1 import Zeta3Domain1
+from ramanujan.poly_domains.Zeta3Domain2 import Zeta3Domain2
+from ramanujan.poly_domains.CartesianProductPolyDomain import CartesianProductPolyDomain
+
 from ramanujan.constants import g_const_dict
-from ramanujan.LHSHashTable import LHSHashTable
-from ramanujan.enumerators.EfficientGCFEnumerator import EfficientGCFEnumerator
+from ramanujan.enumerators.FREnumerator import FREnumerator
 
-# we're gonna have to load all of the domains :(
-from ramanujan.poly_domains.Zeta3Domain1 import Zeta3Domain1
 
-enumerators = {
-    "EfficientGCFEnumerator": EfficientGCFEnumerator
+ENUMERATORS = {
+    "FREnumerator": FREnumerator
 }
-domains = {
-    "Zeta3Domain1": Zeta3Domain1
+DOMAINS = {
+    "Zeta3Domain1": Zeta3Domain1,
+    "Zeta3Domain2": Zeta3Domain2,
+    "CartesianProductPolyDomain": CartesianProductPolyDomain
 }
+RESULTS_FILE_PATH_FORMAT = '{id}_results.json'
 
-class Dummy(object):
-    pass
 
 def get_consts_objects(consts):
     const_objects = []
@@ -32,33 +29,29 @@ def get_consts_objects(consts):
             const_objects.append(g_const_dict[i])
     return const_objects
 
+
 def main():
     json_path = sys.argv[1]
-    bloom_path = sys.argv[2]
+    unique_id = os.path.split(json_path)[1].rsplit('.', 1)[0]
 
     with open(json_path, 'r') as f:
         config = json.load(f)
 
-    lean_lhs = Dummy()
-    with open(bloom_path, 'rb') as f:
-        lean_lhs.bloom = pybloom_live.BloomFilter.fromfile(f)
-
-    poly_domain = domains[config["domain_type"]]()
+    poly_domain = DOMAINS[config["domain_type"]]()
     poly_domain.a_coef_range = config["an_coefs"]
     poly_domain.b_coef_range = config["bn_coefs"]
     poly_domain._setup_metadata()
 
     const_vals = get_consts_objects(config["const_list"])
-
-    enumerator = enumerators[config["enumerator"]](
-        lean_lhs.bloom,
+    enumerator = ENUMERATORS[config["enumerator"]](
         poly_domain,
         const_vals)
 
     results = enumerator.find_initial_hits()
-        print(results)
 
-    # TODO - write results to file
+    with open(RESULTS_FILE_PATH_FORMAT.format(id=unique_id), 'w') as f:
+        json.dump(results, f)
+
 
 if __name__ == '__main__':
     main()
--- scripts/boinc/split_execution.py
+++ scripts/boinc/split_execution.py
@@ -1,12 +1,8 @@
 import os
 import json
 import time
-import ramanujan
 from ramanujan.poly_domains.Zeta3Domain1 import Zeta3Domain1
-from ramanujan.constants import g_const_dict
-from ramanujan.LHSHashTable import LHSHashTable
-from ramanujan.enumerators.EfficientGCFEnumerator import EfficientGCFEnumerator
-from ramanujan.poly_domains.Zeta3Domain1 import Zeta3Domain1
+from ramanujan.poly_domains.Zeta3Domain2 import Zeta3Domain2
 
 
 """
@@ -14,74 +10,61 @@ In this script, we create multiple jobs from a single execution scheme.
 For each job save a json that stores the execution parameters. 
 Those jsons will be sent to different hosts from a BOINC server
 
-The bloom filter is saved 
+Currently, we're planning on distributing FREnumerator only, so there is no support for 
+bloom filter distribution.
 """
-SPLITTED_DOMAIN_CHUNK_SIZE = 100_000
+SPLIT_DOMAIN_CHUNK_SIZE = 100
 BLOOM_LOCAL_PATH = "./{0}_bloom.bin"
-JSON_FORMAT = "./{folder}/{id}.json"
+JSON_NAME_FORMAT = "./{folder}/{filename}.json"
 
-ALLOWD_ENUMERATORS = [
-    "EfficientGCFEnumerator"
+ALLOWED_ENUMERATORS = [
+    "FREnumerator"
 ]
+POLY_DOMAINS_MAPPING = {
+    'Zeta3Domain1': Zeta3Domain1,
+    'Zeta3Domain2': Zeta3Domain2
+}
 
-def dump_bloom_to_file(identifier, lhs):
-    with open(BLOOM_LOCAL_PATH.format(identifier), 'wb') as f:
-        lhs.bloom.tofile(f)
+
+def store_execution_to_json(dest_file_name, enumerator_type, poly_domain, const_list):
+    with open(dest_file_name, 'w') as f: 
+        chunk_data = {
+            "an_coefs": poly_domain.a_coef_range,
+            "bn_coefs": poly_domain.b_coef_range,
+            "enumerator": enumerator_type,
+            "domain_type": poly_domain.__class__.__name__,
+            "const_list": const_list
+        }
+        json.dump(chunk_data, f)
 
 
-def to_json(identifier, enumerator_type, domain, domain_type, const_list):
-    number_of_chunks = poly_search_domain.num_iterations // SPLITTED_DOMAIN_CHUNK_SIZE
-    domain_chunks = poly_search_domain.split_domains_to_processes(number_of_chunks)
+def split_to_jsons(identifier, enumerator_type, domain, const_list):
+    if enumerator_type not in ALLOWED_ENUMERATORS:
+        raise ValueError(f"Required enumerator is not supported. Use one from the following:\n{ALLOWED_ENUMERATORS}")
+    number_of_chunks = domain.num_iterations // SPLIT_DOMAIN_CHUNK_SIZE
+    domain_chunks = domain.split_domains_to_processes(number_of_chunks)
     id_max_len = len(str(len(domain_chunks)))
     os.mkdir(identifier)
 
     for i, chunk in enumerate(domain_chunks):
-        dest_file_name = JSON_FORMAT.format(
+        dest_file_name = JSON_NAME_FORMAT.format(
             folder=identifier, 
-            id=str(i).zfill(id_max_len))
-        with open(dest_file_name, 'w') as f: 
-            chunk_data = {
-                "an_coefs": chunk.a_coef_range,
-                "bn_coefs": chunk.b_coef_range,
-                "enumerator": enumerator_type,
-                "domain_type": domain_type,
-                "const_list": const_list
-            }
-            json.dump(chunk_data,f)
-            
-def get_consts_objects(consts):
-    const_objects = []
-    for i in consts:
-        if isinstance(i, tuple) or isinstance(i, list):
-            const_objects.append(g_const_dict[i[0]](i[1]))
-        else:
-            const_objects.append(g_const_dict[i])
-    return const_objects
+            filename=f"{identifier}_{str(i).zfill(id_max_len)}")
+        store_execution_to_json(dest_file_name, enumerator_type, chunk, const_list)
 
-    
+
 def main():
     # Change the following arguments as you wish 
-    const_list = ['e']
-    saved_hash = 'e_lhs_dept5_db'
-    lhs_search_limit = 5
-    identifier = None # distinct name for generated json
+    const_list = [('zeta', 3)]
+    identifier = None  # distinct name for generated json
 
-    const_objects = get_consts_objects(const_list)
     identifier = identifier if identifier else time.strftime("%y%m%d_%H%M%S")
-    lhs = LHSHashTable(
-        saved_hash,
-        lhs_search_limit,
-        const_objects) 
+    
+    poly_search_domain = Zeta3Domain2(
+        [(1, 100), (1, 100)],
+        (1, 50))
 
-    poly_search_domain = Zeta3Domain1(
-        [(2, 2), (1, 1), (1, 100), (1, 100)],
-        (-50, -1))
-
-    to_json(identifier, "EfficientGCFEnumerator", poly_search_domain, "Zeta3Domain1", const_list)
-    EfficientGCFEnumerator,
-        lhs,
-        poly_search_domain,
-        const_objects
+    split_to_jsons(identifier, "FREnumerator", poly_search_domain, const_list)
 
 
 if __name__ == '__main__':
