7322a952af75b71bc4182e850cd59d3ff5a910b0
Rotem Elimelech <rotelimelech@gmail.com>
2021-06-02 15:55:31 +0300

    making multiprocessing work with FR enumerator without breaking other enumerators

--- scripts/zeta3_fr_results.py
+++ scripts/zeta3_fr_results.py
@@ -1,6 +1,8 @@
 from ramanujan.enumerators.FREnumerator import FREnumerator
 from ramanujan.poly_domains.Zeta3Domain2 import Zeta3Domain2
 from ramanujan.constants import g_const_dict
+from ramanujan.multiprocess_enumeration import multiprocess_enumeration
+
 
 """
 This script enumerates GCFs for zeta(3) using the FR algorithm.
@@ -11,19 +13,31 @@ It uses the following series:
 a(n) = x0(n**3 + (n+1)**3) + x1(2n+1)
 b(n) = x2*n^6
 """
- 
-# define the poly domain
-poly_search_domain = Zeta3Domain2(
-    [(1, 3), (-20, 20)],
-    (1, 2))
 
-# create an enumerator to iter thought the poly domain and compare it to the lhs table
-enumerator = FREnumerator(
-    poly_search_domain,
-    [g_const_dict['zeta'](3)]
-)
 
-results = enumerator.full_execution()
-print("{} results found!".format(len(results)))
-for r in results:
-	print(r)
+def main():
+    # define the poly domain
+    poly_search_domain = Zeta3Domain2(
+        [(1, 3), (-20, 20)],
+        (1, 2))
+
+    # create an enumerator to iter thought the poly domain and compare it to the lhs table
+    enumerator = FREnumerator(
+        poly_search_domain,
+        [g_const_dict['zeta'](3)]
+    )
+
+    # results = enumerator.full_execution()
+    results = multiprocess_enumeration(
+        FREnumerator,
+        None, # No LHS in FREnumerator
+        poly_search_domain,
+        [g_const_dict['zeta'](3)],
+        4)
+
+    print("{} results found!".format(len(results)))
+    for r in results:
+        print(r)
+
+if __name__ == '__main__':
+    main()
