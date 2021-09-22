027150de090017c15ebbb0c1c76b087faf126e30
Rotem Elimelech <rotelimelech@gmail.com>
2021-09-22 14:46:57 +0300

    new optianl parameters to poly domains

--- scripts/boinc/execute_from_json.py
+++ scripts/boinc/execute_from_json.py
@@ -3,6 +3,7 @@ import sys
 import json
 from ramanujan.poly_domains.Zeta3Domain1 import Zeta3Domain1
 from ramanujan.poly_domains.Zeta3Domain2 import Zeta3Domain2
+from ramanujan.poly_domains.Zeta5Domain import Zeta5Domain
 from ramanujan.poly_domains.CartesianProductPolyDomain import CartesianProductPolyDomain
 
 from ramanujan.constants import g_const_dict
@@ -15,6 +16,7 @@ ENUMERATORS = {
 DOMAINS = {
     "Zeta3Domain1": Zeta3Domain1,
     "Zeta3Domain2": Zeta3Domain2,
+    "Zeta5Domain": Zeta5Domain,
     "CartesianProductPolyDomain": CartesianProductPolyDomain
 }
 RESULTS_FILE_PATH_FORMAT = '{id}_results.json'
@@ -40,6 +42,8 @@ def main():
     poly_domain = DOMAINS[config["domain_type"]]()
     poly_domain.a_coef_range = config["an_coefs"]
     poly_domain.b_coef_range = config["bn_coefs"]
+    poly_domain.only_balanced_degress = config["only_balanced_degress"]
+    poly_domain.use_strict_convergence_cond = config["use_strict_convergence_cond"]
     poly_domain._setup_metadata()
 
     const_vals = get_consts_objects(config["const_list"])
