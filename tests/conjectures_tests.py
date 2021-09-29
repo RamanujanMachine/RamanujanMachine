0aa088550f01a781c67ad2b47aad0c844437c58e
Rotem Elimelech <rotelimelech@gmail.com>
2021-09-29 22:37:27 +0300

    test results no longer use 2nd degree of constants

--- tests/conjectures_tests.py
+++ tests/conjectures_tests.py
@@ -237,12 +237,12 @@ class APITests(unittest.TestCase):
 
         self.assertEqual(len(results), 7)
 
-        self.assertIn(((1, 0), (1,), [1, 0, 0], [0, 1, 0]), results)
-        self.assertIn(((1, 4), (1,), [1, 0, 0], [-1, 1, 0]), results)
-        self.assertIn(((1, 12), (1,), [8, 0, 0], [-9, 8, 0]), results)
-        self.assertIn(((2, 3), (2,), [2, 0, 0], [-8, 7, 0]), results)
-        self.assertIn(((2, 15), (2,), [54, 0, 0], [-224, 189, 0]), results)
-        self.assertIn(((3, -2), (1,), [8, 0, 0], [0, 7, 0]), results)
+        self.assertIn(((1, 0), (1,), [1, 0], [0, 1]), results)
+        self.assertIn(((1, 4), (1,), [1, 0], [-1, 1]), results)
+        self.assertIn(((1, 12), (1,), [8, 0], [-9, 8]), results)
+        self.assertIn(((2, 3), (2,), [2, 0], [-8, 7]), results)
+        self.assertIn(((2, 15), (2,), [54, 0], [-224, 189]), results)
+        self.assertIn(((3, -2), (1,), [8, 0], [0, 7]), results)
 
 
 if __name__ == '__main__':
