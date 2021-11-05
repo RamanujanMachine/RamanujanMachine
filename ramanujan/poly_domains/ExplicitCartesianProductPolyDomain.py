5fb8adad6e4c486d2baa7f4c0cd69abfc4229379
Rotem Elimelech <rotelimelech@gmail.com>
2021-11-05 10:31:30 +0200

    adding new line, and cosmetic changes

--- ramanujan/poly_domains/ExplicitCartesianProductPolyDomain.py
+++ ramanujan/poly_domains/ExplicitCartesianProductPolyDomain.py
@@ -7,9 +7,10 @@ class ExplicitCartesianProductPolyDomain(CartesianProductPolyDomain):
     state the required range for each coef individually.
     It's used mostly in splitting a cartesian domain to BOINC clients.
     """
+
     def __init__(self, a_coefs=((0, 0),), b_coefs=((0, 0),), *args, **kwargs):
-        super().__init__(a_deg=len(a_coefs)-1, b_deg=len(b_coefs)-1, a_coef_range=[0, 0], b_coef_range=[0, 0], 
-            *args, **kwargs)
+        super().__init__(a_deg=len(a_coefs) - 1, b_deg=len(b_coefs) - 1, a_coef_range=[0, 0], b_coef_range=[0, 0],
+                         *args, **kwargs)
 
         self._setup_metadata()
 
